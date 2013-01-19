5838a073318b2d503cb88348fba2ca61aad30d8e
oleksandr.baglai <oleksandr.baglai@07780741-d880-df46-bea9-8d6bb61947d5>
2013-01-19 19:01:53 +0000

    Сделал чтобы можно было наконсоли тоже общаться с монстром

--- JavaQuest/src/main/java/apofig/javaquest/console/Runner.java
+++ JavaQuest/src/main/java/apofig/javaquest/console/Runner.java
@@ -21,22 +21,26 @@ public class Runner {
 			printBoard();
 			
 			String line = console.read();
-            if (line.length() != 0) {
+            if (line.length() == 1) {
 				ch = line.charAt(0);
             }
-				
-            if (ch == 115 || ch == 1099) {
-                player.moveDown();
-            } else if (ch == 97 || ch == 1092) {
-                player.moveLeft();
-            } else if (ch == 100 || ch == 1074) {
-                player.moveRight();
-            } else if (ch == 119 || ch == 1094) {
-                player.moveUp();
-            } else if (ch == 'q') {
-                break;
+
+            if (line.length() <= 1) {
+                if (ch == 115 || ch == 1099) {
+                    player.moveDown();
+                } else if (ch == 97 || ch == 1092) {
+                    player.moveLeft();
+                } else if (ch == 100 || ch == 1074) {
+                    player.moveRight();
+                } else if (ch == 119 || ch == 1094) {
+                    player.moveUp();
+                } else if (ch == 'q') {
+                    break;
+                }
+            } else {
+                player.attack(line);
             }
-//            game.tact();
+
 		} while (true);
 		
 		console.print("Game over!");
