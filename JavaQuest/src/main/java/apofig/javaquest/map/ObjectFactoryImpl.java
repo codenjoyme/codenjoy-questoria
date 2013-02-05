477b86b04f709324fbd7337328a38173b05a201f
oleksandr.baglai <oleksandr.baglai@07780741-d880-df46-bea9-8d6bb61947d5>
2013-02-05 19:55:45 +0000

    Выделил ObjectFactoryImpl - готовлю его к созданию фектори монстров

---
+++ JavaQuest/src/main/java/apofig/javaquest/map/ObjectFactoryImpl.java
@@ -0,0 +1,36 @@
+package apofig.javaquest.map;
+
+/**
+ * User: oleksandr.baglai
+ * Date: 2/5/13
+ * Time: 9:52 PM
+ */
+public class ObjectFactoryImpl implements ObjectFactory {
+    private Messages messages;
+
+    public ObjectFactoryImpl(Messages messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public Something make(char c, Place place) {
+        ObjectSettings object = getObject(c);
+        object.setPlace(place);
+        object.setFactory(this);
+        object.setMessages(messages);
+        return (Something)object;
+    }
+
+    private ObjectSettings getObject(char c) {
+        if (c == ' ') {
+            return new Nothing();
+        } else if (c == '@') {
+            return new Monster();
+        } else if (c == '#') {
+            return new Wall();
+        } else if (c == '$') {
+            return new Gold();
+        }
+        throw new UnsupportedOperationException("WTF! New object in world - " + c);
+    }
+}
